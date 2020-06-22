55b4aa74e1ed93aae1bafb4b794904193db0f18e
Dmitry Voronin <d.voronin@sdventures.com>
2020-06-22 17:08:08 +0300

    [*] Axios Demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type State = {
+  postTitles: string[]
+}
+
+const Url = 'https://dev.to'
+
+class App extends React.Component<any, State> {
+  constructor(props: any) {
+    super(props)
+
+    this.setState({
+      postTitles: []
+    })
+  }
+
+  onSubmit = () => {
+    this.getArticles()
+      .then((response) => {
+        if (response.status === 200) {
+          const html = new DOMParser().parseFromString(response.data, 'text/html')
+          const posts = html
+            .getElementById('articles-list')
+            ?.getElementsByClassName('substories')[0]
+            ?.getElementsByClassName('crayons-story')
+
+          if (posts) {
+            let i = 0
+            const postsLength = posts.length
+            let postTitles: string[] = []
+
+            while (i < postsLength) {
+              const post = posts.item(i)
+
+              const title = post
+                ?.getElementsByClassName('crayons-story__title')
+                ?.item(0)
+                ?.getElementsByTagName('a')
+                ?.item(0)
+                ?.text
+
+              if (title) {
+                postTitles = [
+                  ...postTitles,
+                  title
+                ]
+              }
+
+              i++
+            }
+
+            this.setState({
+              postTitles: postTitles
+            })
+          }
+        }
+      }).catch((error) => {
+        console.error(error)
+    })
+  }
+
+  getArticles = () => {
+    return Axios.get(Url)
+  }
+
+  render() {
+    const postTitles = this.state?.postTitles
+
+    return (
+      <>
+        <p>Welcome</p>
+        <input type="button" value="Get articles" onClick={ this.onSubmit }/>
+        {
+          (postTitles && postTitles.length) && postTitles.map((title, index) => <div key={index}>{ title }</div>)
+        }
+      </>
+    );
+  }
 }
 
 export default App;
